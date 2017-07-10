c16fb85090642de7ae7b8540531a9979932b23ab
stvkre <kiariemburuh@gmail.com>
2017-07-10 15:02:18 +0300

    Drawer button -set parameters for the creation of the drawer button

--- app/src/main/java/com/stashley/fitmate/MainActivity.java
+++ app/src/main/java/com/stashley/fitmate/MainActivity.java
@@ -1,13 +1,20 @@
 package com.stashley.fitmate;
 
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mToggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
     }
 }
