2234adee6dad29b38d1bc0b609c4bff80e860cf8
Minneh Mugo <minneh.mugo@gmail.com>
2017-07-10 14:24:56 +0300

    declare FirebaseAuth, AuthStateListener and ProgressDialog

--- app/src/main/java/com/stashley/fitmate/LoginActivity.java
+++ app/src/main/java/com/stashley/fitmate/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.stashley.fitmate;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 
@@ -14,6 +17,10 @@ public class LoginActivity extends AppCompatActivity {
     @Bind(R.id.emailEditText) EditText mEmailEditText;
     @Bind(R.id.passwordEditText) EditText mPasswordEditText;
     @Bind(R.id.passwordLoginButton) Button mPasswordLoginButton;
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private ProgressDialog mAuthProgressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
