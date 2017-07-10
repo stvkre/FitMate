ba425380f2fa82c5b16733619683b592f8615a4c
Minneh Mugo <minneh.mugo@gmail.com>
2017-07-10 14:40:29 +0300

    add logout method to MainActivity

--- app/src/main/java/com/stashley/fitmate/MainActivity.java
+++ app/src/main/java/com/stashley/fitmate/MainActivity.java
@@ -1,7 +1,10 @@
 package com.stashley.fitmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
     }
 }
