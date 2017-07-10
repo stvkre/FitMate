ce3a0408bc6c498296bc6e69d76f5b0ed79c963e
Minneh Mugo <minneh.mugo@gmail.com>
2017-07-10 13:05:07 +0300

    add dependencies to gradle files

--- app/src/main/java/com/stashley/fitmate/CreateAccountActivity.java
+++ app/src/main/java/com/stashley/fitmate/CreateAccountActivity.java
@@ -1,9 +1,10 @@
 package com.stashley.fitmate;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class CreateAccountActivity extends AppCompatActivity {
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
