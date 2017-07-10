7955a354ebefe508ceefe388785e73c8240286ac
Minneh Mugo <minneh.mugo@gmail.com>
2017-07-10 13:06:21 +0300

    bind views to variables in CreateAccountActivity

--- app/src/main/java/com/stashley/fitmate/CreateAccountActivity.java
+++ app/src/main/java/com/stashley/fitmate/CreateAccountActivity.java
@@ -2,9 +2,21 @@ package com.stashley.fitmate;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import butterknife.Bind;
 
 public class CreateAccountActivity extends AppCompatActivity {
     public static final String TAG = CreateAccountActivity.class.getSimpleName();
+
+    @Bind(R.id.createUserButton) Button mCreateUserButton;
+    @Bind(R.id.nameEditText) EditText mNameEditText;
+    @Bind(R.id.emailEditText) EditText mEmailEditText;
+    @Bind(R.id.passwordEditText) EditText mPasswordEditText;
+    @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
+    @Bind(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
