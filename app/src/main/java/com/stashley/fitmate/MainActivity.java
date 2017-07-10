c6d0e132e0d45c0c1b9e973118af6fbca13e7334
Minneh Mugo <minneh.mugo@gmail.com>
2017-07-10 14:45:36 +0300

    add menu item to MainActivity

--- app/src/main/java/com/stashley/fitmate/MainActivity.java
+++ app/src/main/java/com/stashley/fitmate/MainActivity.java
@@ -3,6 +3,8 @@ package com.stashley.fitmate;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
@@ -13,6 +15,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
