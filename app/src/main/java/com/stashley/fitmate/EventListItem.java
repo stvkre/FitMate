3dbd1cfdab4084c5718377db8ea1967cbfb85be9
stvkre <kiariemburuh@gmail.com>
2017-07-12 19:02:57 +0300

    Event list ite class -added constructors and getter methods

--- app/src/main/java/com/stashley/fitmate/EventListItem.java
+++ app/src/main/java/com/stashley/fitmate/EventListItem.java
@@ -9,5 +9,18 @@ public class EventListItem {
     private String head;
     private String description;
 
+    public String getHead() {
+        return head;
+    }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public EventListItem(String head, String description) {
+        this.head = head;
+        this.description = description;
+
+
+    }
 }
