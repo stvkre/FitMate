e525dabbe77a0654a51178243006bea6b9231b8a
stvkre <kiariemburuh@gmail.com>
2017-07-12 18:32:09 +0300

    Java Class -Created new java classes for our activity's functionality

---
+++ app/src/main/java/com/stashley/fitmate/EventListAdapter.java
@@ -0,0 +1,29 @@
+package com.stashley.fitmate;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+/**
+ * Created by stephen on 7/12/17.
+ */
+
+public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder>{
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+    }
+}
---
+++ app/src/main/java/com/stashley/fitmate/EventListItem.java
@@ -0,0 +1,10 @@
+package com.stashley.fitmate;
+
+/**
+ * Created by stephen on 7/12/17.
+ */
+
+public class EventListItem {
+
+
+}
