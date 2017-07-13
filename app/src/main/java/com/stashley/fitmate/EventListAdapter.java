f5c831509b77df67784ca43d3a4e40a168685556
stvkre <kiariemburuh@gmail.com>
2017-07-13 10:59:44 +0300

    Recycler view -created adapter for our recyclerview

--- app/src/main/java/com/stashley/fitmate/EventListAdapter.java
+++ app/src/main/java/com/stashley/fitmate/EventListAdapter.java
@@ -1,29 +1,63 @@
 package com.stashley.fitmate;
 
+import android.app.LauncherActivity;
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 /**
  * Created by stephen on 7/12/17.
  */
 
 public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder>{
+
+    private List<LauncherActivity.ListItem> eventListItems;
+    private Context context;
+
+    public EventListAdapter(List<LauncherActivity.ListItem> eventListItems, Context context) {
+        this.eventListItems = eventListItems;
+        this.context = context;
+    }
+
+    // calling viewholder function
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.event_list_item, parent, false);
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
 
+        LauncherActivity.ListItem listItem = eventListItems.get(position);
+
+//        holder.textViewHead.setText(listItem.getHead());
+//        holder.textViewDescription.setText(listItem.getDescription());
+
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return eventListItems.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
 
+        public TextView textViewHead;
+        public TextView textViewDescription;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            textViewHead = (TextView) itemView.findViewById(R.id.textViewHead);
+            textViewDescription = (TextView) itemView.findViewById(R.id.textViewDescription);
+        }
     }
 }
