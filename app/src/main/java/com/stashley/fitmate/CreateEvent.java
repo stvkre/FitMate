f621bb11cf440f95a959202ccd570fa9b0fe2df4
stvkre <kiariemburuh@gmail.com>
2017-07-18 10:02:09 +0300

    Database -Getting firebase permissions to make directory to store uploaded images

--- app/src/main/java/com/stashley/fitmate/CreateEvent.java
+++ app/src/main/java/com/stashley/fitmate/CreateEvent.java
@@ -1,5 +1,6 @@
 package com.stashley.fitmate;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -9,7 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 
 public class CreateEvent extends AppCompatActivity {
@@ -26,11 +32,19 @@ public class CreateEvent extends AppCompatActivity {
 
     private static final int GALLERY_REQUEST = 1;
 
+    private StorageReference mStorage;
+
+    private ProgressDialog mProgress;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_event);
+
+        // getting firebase permissions to store data
+
+        mStorage = FirebaseStorage.getInstance().getReference();
 
         mSelectImage = (ImageView) findViewById(R.id.imageSelect);
         mCreateEventTitle = (EditText) findViewById(R.id.titleField);
@@ -38,10 +52,13 @@ public class CreateEvent extends AppCompatActivity {
 
         mSubmitBtn = (Button) findViewById(R.id.submitBtn);
 
+        mProgress = new ProgressDialog(this);
+
         mSelectImage.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view) {
 
+                // app accesing device gallery to choose image
 
                 Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
                 galleryIntent.setType("image/*");
@@ -65,10 +82,24 @@ public class CreateEvent extends AppCompatActivity {
 
     private void startPosting() {
 
+        // progress message to show upload status
+
+        mProgress.setMessage("Posting to Events...");
+        mProgress.show();
+
         String title_val = mCreateEventTitle.getText().toString().trim();
         String desc_val = mCreateEventDesc.getText().toString().trim();
 
         if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri !=null){
+
+            StorageReference filepath = mStorage.child("Event_Images").child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                    mProgress.dismiss();
+                }
+            });
 
         }
     }
