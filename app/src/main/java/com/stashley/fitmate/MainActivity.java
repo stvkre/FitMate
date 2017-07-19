2853805e2076459dbbcf70f4d4897de6557f8607
Minneh Mugo <minneh.mugo@gmail.com>
2017-07-19 13:09:48 +0300

    merging with Minneh Branch

--- app/src/main/java/com/stashley/fitmate/MainActivity.java
+++ app/src/main/java/com/stashley/fitmate/MainActivity.java
@@ -9,13 +9,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
@@ -23,7 +22,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -31,11 +30,11 @@ public class MainActivity extends AppCompatActivity {
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
 
-    private List<EventListItem> eventListItems;
-
     private RecyclerView mEvent_list;
 
     private DatabaseReference mDatabase;
+
+    // Drawer activity
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,34 +48,23 @@ public class MainActivity extends AppCompatActivity {
         mEvent_list.setLayoutManager(new LinearLayoutManager(this));
 
 
-//        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
-//        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
-//
-//        mDrawerLayout.addDrawerListener(mToggle);
-//        mToggle.syncState();
-//
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-//
-//
-//        eventListItems = new ArrayList<>();
-//
-//        for(int i = 0; i<=10; i++) {
-//            EventListItem listItem = new EventListItem("fitmate" + i+1,"description");
-//            eventListItems.add(listItem);
-//        }
-//
-//
-//    }
-//
-//
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        if (mToggle.onOptionsItemSelected(item)) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        mToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.open, R.string.close);
+
+        mDrawerLayout.addDrawerListener(mToggle);
+        mToggle.syncState();
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+        }
+
+        // end drawer activity
 
     @Override
     protected void onStart() {
